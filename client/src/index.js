4a4304f2b79f5e8a718318d3b5845ec8873c5d1b
Amin Bashiri <amin.bashiri28@gmail.com>
2021-11-21 23:14:06 +0330

    Initializing drizzle

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// import drizzle functions and contract artifact
+import { Drizzle, generateStore } from "@drizzle/store";
+import MyStringStore from "./contracts/MyStringStore.json";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// let drizzle know what contracts we want and how to access our test blockchain
+const options = {
+  contracts: [MyStringStore],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:9545",
+    },
+  },
+};
+
+// setup the drizzle store and drizzle
+const drizzle = new Drizzle(options);
+
+ReactDOM.render(<App drizzle={drizzle} />, document.getElementById("root"));
