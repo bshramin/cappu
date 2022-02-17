99e87e0560e828e85b0373a19b22c511b3937f63
Amin Bashiri <amin.bashiri28@gmail.com>
2022-02-17 12:58:44 +0330

    add react router

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Web3 from "web3";
+import { Link } from "react-router-dom";
 import { CONTACT_ABI, CONTACT_ADDRESS } from "./config";
 
 function App() {
@@ -7,27 +8,34 @@ function App() {
   const [contract, setContract] = useState();
   const [theString, setTheString] = useState();
 
-  useEffect(() => {
-    async function load() {
-      const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
-      const accounts = await web3.eth.requestAccounts();
-      setAccount(accounts[0]);
+  // useEffect(() => {
+  //   async function load() {
+  //     const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+  //     const accounts = await web3.eth.requestAccounts();
+  //     setAccount(accounts[0]);
 
-      const cappuContract = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
-      setContract(cappuContract);
+  //     const cappuContract = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
+  //     setContract(cappuContract);
 
-      const theString = await cappuContract.methods.getString().call();
-      setTheString(theString);
-    }
+  //     const theString = await cappuContract.methods.getString().call();
+  //     setTheString(theString);
+  //   }
 
-    load();
-  }, []);
+  //   load();
+  // }, []);
 
   return (
     <div>
-      Your account is: {account}
-      <h1>The String</h1>
-      <ul>{theString}</ul>
+      <h1>Bookkeeper</h1>
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
     </div>
   );
 }
--- front/src/index.js
+++ front/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
