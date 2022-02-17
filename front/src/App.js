2cc772d0349659c14bcc536de1174b412f3318a1
Amin Bashiri <amin.bashiri28@gmail.com>
2022-02-17 13:10:43 +0330

    bring back the string

--- front/src/App.js
+++ front/src/App.js
@@ -1,32 +1,9 @@
-import { useEffect, useState } from "react";
-import Web3 from "web3";
 import { Link } from "react-router-dom";
-import { CONTACT_ABI, CONTACT_ADDRESS } from "./config";
 
 function App() {
-  const [account, setAccount] = useState();
-  const [contract, setContract] = useState();
-  const [theString, setTheString] = useState();
-
-  // useEffect(() => {
-  //   async function load() {
-  //     const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
-  //     const accounts = await web3.eth.requestAccounts();
-  //     setAccount(accounts[0]);
-
-  //     const cappuContract = new web3.eth.Contract(CONTACT_ABI, CONTACT_ADDRESS);
-  //     setContract(cappuContract);
-
-  //     const theString = await cappuContract.methods.getString().call();
-  //     setTheString(theString);
-  //   }
-
-  //   load();
-  // }, []);
-
   return (
     <div>
-      <h1>Bookkeeper</h1>
+      <h1>Cappu</h1>
       <nav
         style={{
           borderBottom: "solid 1px",
@@ -34,7 +11,8 @@ function App() {
         }}
       >
         <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
+        <Link to="/expenses">Expenses</Link> |{" "}
+        <Link to="/string">TheString</Link>
       </nav>
     </div>
   );
--- front/src/index.js
+++ front/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
+import Expenses from "./containers/Expenses";
+import Invoices from "./containers/Invoices";
+import HomePage from "./containers/HomePage";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -12,6 +13,7 @@ ReactDOM.render(
       <Route path="/" element={<App />} />
       <Route path="expenses" element={<Expenses />} />
       <Route path="invoices" element={<Invoices />} />
+      <Route path="string" element={<HomePage />} />
     </Routes>
   </BrowserRouter>,
   document.getElementById("root")
