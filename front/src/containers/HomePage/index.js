f7038235e57227d24103060c48196883dba3f333
aminBSHR <amin.bashiri28@gmail.com>
2022-03-25 18:18:12 +0430

    Add description and actions to homepage

--- front/src/containers/HomePage/index.js
+++ front/src/containers/HomePage/index.js
@@ -1,7 +1,17 @@
 import { useEffect, useState } from "react";
+import Button from "@mui/material/Button";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import GitHub from "@mui/icons-material/GitHub";
+import TravelExplore from "@mui/icons-material/TravelExplore";
+
 import Web3 from "web3";
 
-import { CONTACT_ABI, CONTACT_ADDRESS, NETWORK } from "../../config";
+import {
+  CONTACT_ABI,
+  CONTACT_ADDRESS,
+  NETWORK,
+  NETWORK_NAME,
+} from "../../config";
 import "./style.css";
 
 export default function HomePage() {
@@ -29,6 +39,24 @@ export default function HomePage() {
 
   return (
     <div className="homepage-container">
+      <div className="homepage-description">
+        Cappu is an NFT platform helping users mint and transfer data NFT
+        tokens.
+        <br />
+        It is an open-source, work in progress, experimental ERC721
+        smart-contract.
+        <br />
+        <br />
+        This front-end currently contacts the contract deployed on the{" "}
+        {NETWORK_NAME} network, at this address:
+        <br />
+        {CONTACT_ADDRESS}
+        <br />
+        <br />
+        You need to have the Metamask extention installed and choose the{" "}
+        {NETWORK_NAME} network, then you can connect your wallet and start using
+        the platform.
+      </div>
       <div className="homepage-info">
         {numberOfMintedTokens ? (
           <div className="homepage-info-item">
@@ -50,6 +78,35 @@ export default function HomePage() {
             </div>
           </div>
         ) : null}
+      </div>
+      <div className="homepage-actions">
+        <ButtonGroup
+          variant="contained"
+          aria-label="outlined primary button group"
+        >
+          <Button
+            startIcon={<GitHub fontSize="large" color="background" />}
+            onClick={() =>
+              window.open("https://github.com/bshramin/cappu", "_blank")
+            }
+          >
+            View on GitHub
+          </Button>
+          <Button
+            onClick={() =>
+              window.open(
+                "https://" +
+                  NETWORK_NAME +
+                  ".etherscan.io/address/" +
+                  CONTACT_ADDRESS,
+                "_blank"
+              )
+            }
+            startIcon={<TravelExplore fontSize="large" color="background" />}
+          >
+            Explore on EtherScan
+          </Button>
+        </ButtonGroup>
       </div>
     </div>
   );
